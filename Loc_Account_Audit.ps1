14718d968db267d401307a1c41a7f2d1afeeb960
afrosecops <60303770+afrosecops@users.noreply.github.com>
2020-01-25 20:19:28 -0600

    Create Loc_Account_Audit.ps1
    This script allows you to audit local accounts on computers joined to an AD domain

---
+++ Loc_Account_Audit.ps1
@@ -0,0 +1,19 @@
+﻿#Check that the computer exists within your domain
+$testcomp = Get-ADComputer -filter {Enable -eq $True} -Properties Name
+
+#Loop through the computers and get the names of the computers
+foreach ($comp in $testcomp){
+$comp1 = Get-ADComputer $comp -Properties *
+$comp2 = $comp1.Name
+
+#Enumerate the local accounts on the system
+If($comp){
+    $user = Get-LocalUser
+    $localuser = $user.Name
+
+}
+
+#Display each computer and the local accounts that exist on it
+Write-Output "Computer: $comp2 `n Accounts: $localuser`n"
+
+}
