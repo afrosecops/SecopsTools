99b4db181b40c77bb2987bba2c2d785f3cf84223
afrosecops <60303770+afrosecops@users.noreply.github.com>
2020-01-25 20:27:22 -0600

    Create Test_Auth.ps1
    This script allows you to test the validity of a user credentials by attempting to authenticate to a domain controller.
    Input csv file

---
+++ Test_Auth.ps1
@@ -0,0 +1,27 @@
+﻿#Import the usernames and passwords from a csv file on your desktop
+$loggedinuser = $env:USERNAME
+$credcsv = Import-csv -Path "C:\Users\$loggedinuser\Desktop\InputFile.csv"
+
+#Function to test authentication
+Function Test-Auth {
+Param($username, $password)
+(New-Object directoryservices.directoryentry "",$username,$password).psbase.name -ne $null
+
+}
+
+#Loop through the users, passwords and test the authentication status using username and password as parameters
+foreach ($cred in $credcsv){
+$users =  $cred.Username
+$pwds = $cred.Password
+
+foreach($user in $users){
+
+    foreach($pwd in $pwds){}
+
+#Show the result of the authentication attempt
+    Write-Output "$user authentication status" | Test-Auth $user $pwd
+
+
+    }
+
+}
