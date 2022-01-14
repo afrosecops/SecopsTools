7620d2843eb2ab000e6f63a3e047289fdff74bb4
Kader Tapsoba <60303770+afrosecops@users.noreply.github.com>
2022-01-13 19:30:11 -0600

    Create Arrays.ps1

---
+++ Arrays.ps1
@@ -0,0 +1,16 @@
+#Ask the user for input
+$input = Read-Host "Enter string"
+
+#Remove all space of input and store new variable
+#reference: https://docs.microsoft.com/en-us/dotnet/api/system.stringsplitoptions?view=net-6.0
+$inp2 = $input.Split('',[System.StringSplitOptions]::RemoveEmptyEntries) -join ''
+$inp2
+
+#Convert varaiable to array of characters
+$arr = $inp2.ToCharArray()
+
+$sec = $arr | Select-Object -Unique
+$sec
+#$dups = Compare-Object -ReferenceObject $sec -DifferenceObject $arr
+
+#$dups
